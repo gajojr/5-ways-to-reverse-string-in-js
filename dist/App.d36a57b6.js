40d3569f6b91d2245ccd4452aa5d9f6db73b4968
Andrija <andreccccc12@gmail.com>
2020-09-10 18:42:15 +0200

    fixed footer problem with mobile phones

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -28593,7 +28593,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49993" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62620" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
